ef34de00b6aa5be1b8401663ee5f4c12891e0a6d
Dkriss <89033170+Dkriss@users.noreply.github.com>
2023-12-03 22:12:18 +0700

    masih belum bisa, kurang

--- Check Out/Check Out/DataCheckout.js
+++ Check Out/Check Out/DataCheckout.js
@@ -35,4 +35,28 @@ document.addEventListener('DOMContentLoaded', function () {
         // Mengarahkan pengguna ke halaman checkin.html
         window.location.href = 'checkin.html';
     });
+
+    // Menambahkan event listener pada tombol "Bayar"
+    const bayarButton = document.querySelector('button[type="button"]');
+    bayarButton.addEventListener('click', function () {
+        // Mendapatkan data dari formulir
+        const email = document.getElementById('email').value;
+        const title = document.getElementById('title').value;
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const phone = document.getElementById('phone').value;
+
+        // Membuat objek data
+        const formData = {
+            email: email,
+            title: title,
+            firstName: firstName,
+            lastName: lastName,
+            phone: phone
+        };
+
+        // Mengonversi objek data menjadi JSON
+        const formDataJSON = JSON.stringify(formData);
+
+    });
 });
