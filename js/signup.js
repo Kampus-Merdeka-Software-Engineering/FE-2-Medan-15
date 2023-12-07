edaf4c7b0d7808696bbdadcb335fce4f8e2ff31c
Setianto313 <149598567+Setianto313@users.noreply.github.com>
2023-12-07 14:38:49 +0700

    Add files via upload

--- js/signup.js
+++ js/signup.js
@@ -1,13 +1,83 @@
-
-document.getElementById('signupForm').addEventListener('submit', function(event) {
+// fetch('http://localhost:3000/registration')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log({
+//             data
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+function postRegFormData(event) {
     event.preventDefault();
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    if (localStorage.getItem(email) !== null) {
-        alert('Email sudah terdaftar! Silahkan masuk atau gunakan email lain.');
-    } else {
-        localStorage.setItem(email, password);
-        alert('Akun berhasil dibuat! Silahkan masuk.');
-        window.location.href = 'signin.html';
-    }
-});
+}
+
+// Menambahkan pengecekan apakah elemen dengan ID 'signupForm' ada sebelum menambahkan event listener
+const signupForm = document.getElementById('signupForm');
+if (signupForm) {
+    signupForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        fetch('http://localhost:3000/registration', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, email, password }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message);
+            if (data.message.includes('berhasil')) {
+                window.location.href = 'signin.html';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+} else {
+    console.error("Element with ID 'signupForm' not found.");
+}
+
+
+
+// function postRegFormData(event) {
+//     event.preventDefault();
+// }
+
+// document.getElementById('signupForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     const username = document.getElementById('username').value;
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+
+//     fetch('http://localhost:3000/registration', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username, email, password }),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         alert(data.message);
+//         if (data.message.includes('berhasil')) {
+//             window.location.href = 'signin.html';
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// });
+
+// Menghapus baris ini, karena tidak dibutuhkan
+// document.querySelector('signupForm').addEventListener('Masuk', postRegFormData);
+
+// document.querySelector('signupForm').addEventListener('Masuk', postRegFormData);
+// document.getElementById('signupForm').addEventListener('submit', postRegFormData);
